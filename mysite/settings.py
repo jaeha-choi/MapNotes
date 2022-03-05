95a9a746734fab7e4b764d3d3313227914614a37
Jay Lin <jial8@uw.edu>
2022-03-05 00:48:49 -0800

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mapnotes.apps.MapnotesConfig'
+    'mapnotes.apps.MapnotesConfig',
     'whitenoise.runserver_nostatic',
 ]
 
